56714ac39e98292ff74c94be958a6b7cf28464b1
chrissye0 <christinee1220@gmail.com>
2025-10-20 21:11:47 -0400

    edited ir code to better detect movement

--- ir-test/src/main.cpp
+++ ir-test/src/main.cpp
@@ -1,29 +1,42 @@
 // #include <Arduino.h>
-// #include <IRremote.h>
 
-// const int pin = 12;
-
+// const int sensorPin = 2;
+// int lastState = LOW;
+// unsigned long lastChange = 0;
+// int test = 0;
 
 // void setup() {
 //   Serial.begin(9600);
-//   IrReceiver.begin(pin, ENABLE_LED_FEEDBACK);
+//   pinMode(sensorPin, INPUT);
 // }
 
 // void loop() {
-//   if (IrReceiver.decode()) {
-//       Serial.println("detected!");
-//       Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX); // Print "old" raw data
-//       IrReceiver.printIRResultShort(&Serial); // Print complete received data in one line
-//       IrReceiver.resume(); // Enable receiving of the next value
+//   int currentState = digitalRead(sensorPin);
+//   Serial.println(currentState);
+//   // detect change in sensor state
+//   if (currentState != lastState) {
+//     lastChange = millis();
+//     Serial.println("Movement detected!");
+//     // to show difference in printed messages
+//     test++;
+//     // Serial.println(test);
 //   }
+//   lastState = currentState;
+//   delay(20);
 // }
 
 #include <Arduino.h>
 
 const int sensorPin = 2;
 int lastState = LOW;
-unsigned long lastChange = 0;
-int test = 0;
+unsigned long lastChangeTime = 0;
+unsigned long windowStart = 0;
+int changeCount = 0;
+bool moving = false;
+
+const unsigned long windowDuration = 1000;  // 1 second window
+const int movementThreshold = 5;            // Require at least 5 flips/sec to count as movement
+const unsigned long idleTimeout = 1200;     // Time with no flips = movement stopped
 
 void setup() {
   Serial.begin(9600);
@@ -33,14 +46,28 @@ void setup() {
 void loop() {
   int currentState = digitalRead(sensorPin);
 
-  // detect change in sensor state
+  // Detect state change
   if (currentState != lastState) {
-    lastChange = millis();
-    Serial.println("Movement detected!");
-    // to show difference in printed messages
-    test++;
-    Serial.println(test);
+    lastChangeTime = millis();
+    changeCount++;
   }
+
+  // Every second, evaluate the number of flips
+  if (millis() - windowStart >= windowDuration) {
+    if (!moving && changeCount >= movementThreshold) {
+      Serial.println("Movement detected!");
+      moving = true; // Mark movement burst as started
+    }
+    // Reset for next time window
+    changeCount = 0;
+    windowStart = millis();
+  }
+
+  // If no flips for a while, end movement burst
+  if (moving && millis() - lastChangeTime > idleTimeout) {
+    moving = false;
+  }
+
   lastState = currentState;
   delay(20);
 }
