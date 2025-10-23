ec9c81b0e2f769092cc9793ac17060b3c893e501
kestrelw <mail4melodie@gmail.com>
2025-10-23 14:13:01 -0400

    star rating

--- brushing-detector-server/client/end-screen.js
+++ brushing-detector-server/client/end-screen.js
@@ -6,13 +6,14 @@ const init = () => {
     const teethCleaned = Number(localStorage.getItem("totalTeeth")) || 0;
     const bactCount = Number(localStorage.getItem("totalBact")) || 0;
 
-    const score = document.getElementById('finalScore');
+    const score = 2000//document.getElementById('finalScore');
     score.textContent = points.toLocaleString();;
 
     const starRating = () => {
-        if (points >= 7000) return 3;
-        if (points >= 3000) return 2;
-        return 1;
+        if (score >= 8000) return 3;
+        else if(score<2000) return 0;
+        console.log("starCount "+((Math.floor(score/1000))-2)*.5);
+        return ((Math.floor(score/1000))-2)*.5;
     };
 
     const starsContainer = document.querySelector('.stars');
@@ -24,15 +25,51 @@ const init = () => {
     stat1Heading.textContent = bactCount;
     stat2Heading.textContent = teethCleaned;
 
-  const numStars = starRating();
-
-  for (let i = 0; i < numStars; i++) {
-    const star = document.createElement('img');
-        star.src = 'game-page-assets/Star.svg';
+    
+    const createFullStar = () => {
+      const star = document.createElement('img');
+        star.src = 'game-page-assets/star.svg';
         star.alt = 'star';
         star.classList.add('starImg');
-    starsContainer.appendChild(star);
-  }
+      starsContainer.appendChild(star);
+    };
+
+    const createHalfStar = () => {
+      const star = document.createElement('img');
+        star.src = 'game-page-assets/starHalf.svg';
+        star.alt = 'star';
+        star.classList.add('starImg');
+      starsContainer.appendChild(star);
+    };
+
+    const createEmptyStar = () => {
+      const star = document.createElement('img');
+        star.src = 'game-page-assets/starEmpty.svg';
+        star.alt = 'star';
+        star.classList.add('starImg');
+      starsContainer.appendChild(star);
+    };
+
+    const numStars = starRating();
+    console.log(numStars);
+
+    for(let i = Math.floor(numStars); i>0; i--){
+      createFullStar();
+    }
+    if(!Number.isInteger(numStars)){
+      createHalfStar();
+    }
+    for(let i = 3-Math.ceil(numStars); i>0; i--){
+      createEmptyStar();
+    }
+
+    
+  // }
+
+
+  // document.getElementById('restartButton').addEventListener('click', function() {
+  //   this.style.backgroundImage = 'url("restartPress")';
+  // });
 };
 
 
