3d94757ce59a6c4beef9b72191dc3adf1cc0bff6
kestrelw <mail4melodie@gmail.com>
2025-11-13 14:52:54 -0500

    tutorial v3

--- brushing-detector-server/client/tutorial-page.js
+++ brushing-detector-server/client/tutorial-page.js
@@ -2,10 +2,15 @@ const init = () => {
     
   const video = document.getElementById("tutorial-video-1");
   const secondVideo = document.getElementById("tutorial-video-2");
+  const thirdVideo = document.getElementById("tutorial-video-3");
 
   secondVideo.pause();
   secondVideo.currentTime = 0;
   secondVideo.style.zIndex = "0";
+
+  thirdVideo.pause();
+  thirdVideo.currentTime = 0;
+  thirdVideo.style.zIndex = "-1";
 
   video.addEventListener("ended", () => {
     playSecondVideo();
@@ -13,6 +18,10 @@ const init = () => {
   });
 
   secondVideo.addEventListener("ended", () => {
+    playThirdVideo();
+  });
+
+  thirdVideo.addEventListener("ended", () => {
     window.location.href = "game-page.html";
   });
 
@@ -21,7 +30,10 @@ const init = () => {
       if (secondVideo.style.zIndex === "0") {
         // If we're on the first video, jump to second
         playSecondVideo();
-      } else {
+      } else if (thirdVideo.style.zIndex === "-1") {
+        // If we're on the first video, jump to second
+        playThirdVideo();
+      } else{
         // If we're already on the second, go to game page
         window.location.href = "game-page.html";
       }
@@ -32,6 +44,12 @@ const init = () => {
     secondVideo.currentTime = 0;
     secondVideo.style.zIndex = "2";
     secondVideo.play();
+  }
+
+  function playThirdVideo() {
+    thirdVideo.currentTime = 0;
+    thirdVideo.style.zIndex = "5";
+    thirdVideo.play();
   }
 
 
