b443e0891771ce8bda4cd804266118c56a5c061f
Nemesis Velazquez <nav7977@rit.edu>
2026-01-31 12:39:26 -0500

    updated connect file with new ports for arduino 2- EVERYONE PLS ADD UR OWN PORTS

--- brushing-detector-server/src/arduino-connect.js
+++ brushing-detector-server/src/arduino-connect.js
@@ -2,14 +2,24 @@ const { SerialPort } = require('serialport')
 const { ReadlineParser } = require('@serialport/parser-readline')
 
 const connect = (handler) => {
-  let port = new SerialPort({ path: 'COM11', baudRate: 9600 }); //Neme's port
+  //BRUSHING PORTS
+  let port = new SerialPort({ path: 'COM10', baudRate: 9600 }); //Neme's port
   // let port = new SerialPort({ path: 'COM3', baudRate: 9600 }); //kash's port
   //let port = new SerialPort({ path: '/dev/tty.usbmodemC04E30135F882', baudRate: 9600 }); // for mac testing, CHANGE FOR WINDOWS
 
+  //FLOSSING PORTS - EVERYONE ADD
+  let port2 = new SerialPort({ path: 'COM11', baudRate: 9600 }); //Neme's port
+  //let port2 = Kash's port (maybe??)
+  //let port2 = Chrissy's shitty mac port
+
   const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
+  const parser2 = port2.pipe(new ReadlineParser({ delimiter: '\r\n' }))
 
   port.on('open', () => {
     console.log('Serial port opened. Listening...\n')
+  })
+  port2.on('open', () => {
+    console.log('Serial port2 opened. Listening...\n')
   })
 
   parser.on('data', (line) => {
@@ -21,13 +31,20 @@ const connect = (handler) => {
     handler(line)
 
   });
+    parser2.on('data', (line) => {
+    handler(line) // SAME handler, same behavior
+  })
 
   port.on('error', (err) => {
     console.log('💣 Could not connect to Arduino on port COM3');
     console.log(err);
   });
+  port2.on('error', (err) => {
+    console.log('💣 Could not connect to Arduino 2')
+    console.log(err)
+  })
 
-  return port
+  return { port, port2 }
 
 }
 
