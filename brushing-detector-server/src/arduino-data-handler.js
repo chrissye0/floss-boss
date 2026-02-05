79406f6bef5107bba3658bf959bc01c30d4a0c0f
Kash <kka7705@g.rit.edu>
2026-02-05 17:09:55 -0500

    BRUSHING WORKING (INSERT FREAKY SONIC GIF)

--- brushing-detector-server/src/arduino-data-handler.js
+++ brushing-detector-server/src/arduino-data-handler.js
@@ -7,11 +7,19 @@ let activeToothIndex = null;
 
 // Thresholds
 // Light over sensor = active
-const SENSOR_THRESHOLDS = [0.94, 0.09, 0.09, 0.07, 0.07, 0.07];
+//go a bit higher for high thresholds (should be higher than a reading so we do not
+//trip it with noise)
+//and for low thresholds enough when we take away the light 
+//for high we can do 0.9 and for low we can do 0.6 for example to account for noise for light even though
+//the low is 0.4 and the high is 0.9 something 
+//look for the point of where we are at ambient light for the low threshold 
+const HIGH_SENSOR_THRESHOLDS = [0.90, 0.90, 0.99, 0.99, 0.99, 0.99];
+const LOW_SENSOR_THRESHOLDS = [0.30, 0.30, 0, 0, 0, 0];
 // Small change = brushing
 const MOTION_THRESHOLD = 0.0001;
 let detectedTooth = null;
 let brushingDetected = false;
+let toothDetected = 0;
 
 let lastLogTime = 0;
 const LOG_INTERVAL = 50;
@@ -22,8 +30,10 @@ let sensorDeltaReadingsIndex = 0;
 let numReadingsToKeep = 5;
 
 
+
+
 const handleData = (data) => {
-  detectedTooth = null;
+  //detectedTooth = null;
   let frameTime = performance.now() - timeLastSensorReading;
   //how much time ha spassed since last sensor reading
   timeLastSensorReading = performance.now();
@@ -33,15 +43,28 @@ const handleData = (data) => {
   for (let i = 0; i < sensorValues.length; i++) {
     const current = sensorValues[i];
 
-    console.log('current', current);
+    //console.log('current', current);
 
     // Detect the first active tooth
-    if (current > SENSOR_THRESHOLDS[i]) {
+    if (current > HIGH_SENSOR_THRESHOLDS[i]) {
       console.log('Active tooth', i);
+      //if we do not have a tooth currently and the current tooth is not a detected tooth then
+      //leave the loop
+      if(detectedTooth !== null && i !== detectedTooth) {
+        break;
+      }
       detectedTooth = i;
+      toothDetected = performance.now();
 
       break;
     }
+
+    console.log(performance.now() - toothDetected)
+    //only allowed to turn it off if we are on a tooth and below the threshold for quarter a second 
+    if(detectedTooth === i && current < LOW_SENSOR_THRESHOLDS[i] && performance.now() - toothDetected > 250){
+      detectedTooth = null;
+    }
+
   }
 
   // Update game state
