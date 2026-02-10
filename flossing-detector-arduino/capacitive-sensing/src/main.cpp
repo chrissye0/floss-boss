543f5fa3d6d39ed22a2d39f8699573e2e99af08b
Nemesis Velazquez <nav7977@rit.edu>
2026-02-10 09:40:30 -0500

    Ported over new flossing arduino file

--- flossing-detector-arduino/capacitive-sensing/src/main.cpp
+++ flossing-detector-arduino/capacitive-sensing/src/main.cpp
@@ -5,10 +5,10 @@
 //ports to be read
 //CapacitiveSensor capSensor = CapacitiveSensor(yellow,green);
 CapacitiveSensor capSensor = CapacitiveSensor(3,2);
-//CapacitiveSensor capSensor2 = CapacitiveSensor(5,4);
-
-//wires on the floor of con might affect sensitivity!!
-int threshold = 800;
+CapacitiveSensor capSensor2 = CapacitiveSensor(5,4);
+CapacitiveSensor capSensor3 = CapacitiveSensor(7,6);
+CapacitiveSensor capSensor4 = CapacitiveSensor(9,8);
+CapacitiveSensor capSensor5 = CapacitiveSensor(11,10);
 
 void setup() {
   Serial.begin(9600);
@@ -16,17 +16,18 @@ void setup() {
 
 void loop() {
   //variables to read sensors
-  long sensorValue = capSensor.capacitiveSensor(30);
-  //long sensorValue2 = capSensor2.capacitiveSensor(30);
+  long sensorValue = capSensor.capacitiveSensor(100);
+  long sensorValue2 = capSensor2.capacitiveSensor(100);
+  long sensorValue3 = capSensor3.capacitiveSensor(100);
+  long sensorValue4 = capSensor4.capacitiveSensor(100);
+  long sensorValue5 = capSensor5.capacitiveSensor(100);
 
   //debugging
   Serial.println(sensorValue);
-   //Serial.println(sensorValue2);
-
-  //response
-  if(sensorValue > threshold /*|| sensorValue2 > threshold*/) {
-    Serial.println("Flossing");
-  }
+  Serial.println(sensorValue2);
+  Serial.println(sensorValue3);
+  Serial.println(sensorValue4);
+  Serial.println(sensorValue5);
  
   delay(100); 
 }
