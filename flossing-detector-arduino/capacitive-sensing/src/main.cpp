2cb1d9232bb1a5523fa8c43a5b9e927e3975ad46
Nemesis Velazquez <nav7977@rit.edu>
2026-02-12 09:54:56 -0500

    fixed output loop

--- flossing-detector-arduino/capacitive-sensing/src/main.cpp
+++ flossing-detector-arduino/capacitive-sensing/src/main.cpp
@@ -9,30 +9,36 @@ CapacitiveSensor capSensor2 = CapacitiveSensor(5, 4);
 CapacitiveSensor capSensor3 = CapacitiveSensor(7, 6);
 CapacitiveSensor capSensor4 = CapacitiveSensor(9, 8);
 CapacitiveSensor capSensor5 = CapacitiveSensor(11, 10);
-CapacitiveSensor capSensor5 = CapacitiveSensor(13, 12);
+CapacitiveSensor capSensor6 = CapacitiveSensor(13, 12);
+
+const int NUM_SENSORS = 6;
+
+CapacitiveSensor* sensors[NUM_SENSORS] = {
+  &capSensor,
+  &capSensor2,
+  &capSensor3,
+  &capSensor4,
+  &capSensor5,
+  &capSensor6
+};
 
 void setup()
 {
   Serial.begin(9600);
 }
 
-void loop()
-{
-  // variables to read sensors
-  long sensorValue = capSensor.capacitiveSensor(100);
-  long sensorValue2 = capSensor2.capacitiveSensor(100);
-  long sensorValue3 = capSensor3.capacitiveSensor(100);
-  long sensorValue4 = capSensor4.capacitiveSensor(100);
-  long sensorValue5 = capSensor5.capacitiveSensor(100);
-  long sensorValue6 = capSensor6.capacitiveSensor(100);
+void loop() {
 
-  // debugging
-  Serial.println(sensorValue);
-  Serial.println(sensorValue2);
-  Serial.println(sensorValue3);
-  Serial.println(sensorValue4);
-  Serial.println(sensorValue5);
-  Serial.println(sensorValue6);
+  for (int i = 0; i < NUM_SENSORS; i++) {
+    long sensorValue = sensors[i]->capacitiveSensor(100);
+    Serial.print(sensorValue);
+
+    if (i < NUM_SENSORS - 1) {
+      Serial.print(",");
+    }
+  }
+
+  Serial.println();
 
   delay(100);
 }
