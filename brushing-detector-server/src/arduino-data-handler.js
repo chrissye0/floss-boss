ada8ecca4997f9aa4da0d2a009db91a012aea952
Kash <kka7705@g.rit.edu>
2026-02-15 14:56:32 -0500

    changed thresholds

--- brushing-detector-server/src/arduino-data-handler.js
+++ brushing-detector-server/src/arduino-data-handler.js
@@ -12,8 +12,8 @@ let activeToothIndex = null;
 //the low is 0.4 and the high is 0.9 something
 //look for the point of where we are at ambient light for the low threshold
 //put at 0 if not testing that tooth for low and for high put 0.99 if not testing for the specific tooth
-const HIGH_SENSOR_THRESHOLDS = [0.9, 0.9, 0.99, 0.99, 0.99, 0.99];
-const LOW_SENSOR_THRESHOLDS = [0.5, 0.5, 0.5, 0.5, 0.5, 0.];
+const HIGH_SENSOR_THRESHOLDS = [0.9, 0.9, 0.9, 0.9, 0.9, 0.9];
+const LOW_SENSOR_THRESHOLDS = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5];
 // Small change = brushing
 const MOTION_THRESHOLD = 0.0001;
 let detectedTooth = null;
