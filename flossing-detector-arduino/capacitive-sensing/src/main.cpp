ebe316dd397264a12cdb7543da8f0923cd5e4e9b
kestrelw <mail4melodie@gmail.com>
2026-03-01 13:24:21 -0500

    is there new stuff or did i already push last time...?

--- flossing-detector-arduino/capacitive-sensing/src/main.cpp
+++ flossing-detector-arduino/capacitive-sensing/src/main.cpp
@@ -6,35 +6,32 @@
 // CapacitiveSensor capSensor = CapacitiveSensor(yellow,green);
 CapacitiveSensor capSensor = CapacitiveSensor(3, 2);
 CapacitiveSensor capSensor2 = CapacitiveSensor(5, 4);
-// CapacitiveSensor capSensor3 = CapacitiveSensor(7, 6);
-// CapacitiveSensor capSensor4 = CapacitiveSensor(9, 8);
-// CapacitiveSensor capSensor5 = CapacitiveSensor(11, 10);
-// CapacitiveSensor capSensor6 = CapacitiveSensor(13, 12);
+CapacitiveSensor capSensor3 = CapacitiveSensor(7, 6);
+CapacitiveSensor capSensor4 = CapacitiveSensor(9, 8);
+CapacitiveSensor capSensor5 = CapacitiveSensor(11, 10);
+CapacitiveSensor capSensor6 = CapacitiveSensor(13, 12);
 
-const int NUM_SENSORS = 2;
+const int NUM_SENSORS = 6;
 
 CapacitiveSensor* sensors[NUM_SENSORS] = {
   &capSensor,
   &capSensor2,
-  // &capSensor3,
-  // &capSensor4,
-  // &capSensor5,
-  // &capSensor6
+  &capSensor3,
+  &capSensor4,
+  &capSensor5,
+  &capSensor6
 };
 
 void setup()
 {
   Serial.begin(9600);
-     for (int i = 0; i < NUM_SENSORS; i++) {
-       sensors[i]->set_CS_Timeout_Millis(200);     // turn off autocalibrate on channel 1 - just as an example
-     }
 
 }
 
 void loop() {
 
   for (int i = 0; i < NUM_SENSORS; i++) {
-    long sensorValue = sensors[i]->capacitiveSensorRaw(50);
+    long sensorValue = sensors[i]->capacitiveSensor(50);
     Serial.print(sensorValue);
 
     if (i < NUM_SENSORS - 1) {
